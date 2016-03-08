1d6c5f0523c3b6ee9088ed716efeaeadec799651
Christopher N. Hesse <raymanfx@gmail.com>
2016-03-08 23:10:50 +0100

    trelte: Decommonize audio configs
    Change-Id: I6a57ea69a354f1907bbd2e4ad2641601be8b399e

--- device-common.mk
+++ device-common.mk
@@ -138,8 +138,7 @@ PRODUCT_COPY_FILES += \
 ###########################################################
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
