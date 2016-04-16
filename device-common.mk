055c4260106f4f6cd36ae186eecb890a288cd49d
Christopher N. Hesse <raymanfx@gmail.com>
2016-04-16 21:31:02 +0200

    trelte: Update properties from N910CXXU2DPCB
    Change-Id: I5ab2eb91d2cd6789880ff281bec398b22dc5da24

--- device-common.mk
+++ device-common.mk
@@ -93,8 +93,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/lib/libsec-ril.so \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10 \
+    ro.ril.telephony.mqanelements=5 \
+    ro.telephony.default_network=9 \
     ro.telephony.ril_class=SlteRIL \
-    ro.ril.telephony.mqanelements=5
+    persist.radio.apm_sim_not_pwdn=1
 
 ###########################################################
 ### WIFI
@@ -242,7 +244,9 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 ###########################################################
 
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.sys.fw.dex2oat_thread_count=4
+    ro.sys.fw.dex2oat_thread_count=4 \
+    dalvik.vm.image-dex2oat-filter=speed \
+    dalvik.vm.dex2oat-filter=speed
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
@@ -259,15 +263,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=88 \
     ro.hwui.layer_cache_size=58 \
-    ro.hwui.path_cache_size=32 \
+    ro.hwui.path_cache_size=16 \
+    ro.hwui.texture_cache_flushrate=0.4 \
     ro.hwui.shape_cache_size=4 \
     ro.hwui.gradient_cache_size=2 \
-    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.drop_shadow_cache_size=6 \
     ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=4096 \
-    ro.hwui.text_large_cache_height=4096
+    ro.hwui.text_large_cache_height=2048 \
+    ro.hwui.fbo_cache_size=16
 
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
