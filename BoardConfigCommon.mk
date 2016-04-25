271fa0864d46723ed7df84b4d10caf06c7104ecb
Christopher N. Hesse <raymanfx@gmail.com>
2016-04-25 18:19:56 +0200

    trelte: Chose the best CPU variant for big.LITTLE
    Change-Id: I48110794ca3d3ad4ba46653469590dfea3bfbcb1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,7 +13,7 @@ TARGET_SOC := exynos5433
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a15
+TARGET_CPU_VARIANT := cortex-a53.a57
 
 # big.LITTLE load balancing
 ENABLE_CPUSETS := true
