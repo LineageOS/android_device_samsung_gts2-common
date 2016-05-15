64534609766b345fa6f0901cad9fb7a1ce8f73f7
Christopher N. Hesse <raymanfx@gmail.com>
2016-05-15 21:36:03 +0200

    trelte: Move properties to separate mk
    Change-Id: I7296a851c585183057bf5cde352ae7c6b17ff9a4

--- device-common.mk
+++ device-common.mk
@@ -56,11 +56,6 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 560dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196609 \
-    ro.sf.lcd_density=560 \
-    ro.bq.gpu_to_cpu_unsupported=1
-
 PRODUCT_PACKAGES += \
     gralloc.exynos5
 
@@ -87,16 +82,6 @@ PRODUCT_PACKAGES += \
     libsecril-client \
     libsecril-client-sap \
     modemloader
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libargs=-d /dev/ttyS0 \
-    rild.libpath=/system/lib/libsec-ril.so \
-    ro.ril.hsxpa=1 \
-    ro.ril.gprsclass=10 \
-    ro.ril.telephony.mqanelements=5 \
-    ro.telephony.default_network=9 \
-    ro.telephony.ril_class=SlteRIL \
-    persist.radio.apm_sim_not_pwdn=1
 
 ###########################################################
 ### WIFI
@@ -135,9 +120,6 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.nfc.sec_hal=true
-
 ###########################################################
 ### AUDIO
 ###########################################################
@@ -151,10 +133,6 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     audio.primary.universal5433
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1 \
-    audio_hal.force_voice_config=wide
 
 ###########################################################
 ### OMX/MEDIA
@@ -199,9 +177,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
-
 ###########################################################
 ### TOUCHSCREEN
 ###########################################################
@@ -239,41 +214,8 @@ PRODUCT_PACKAGES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.debug_level=0x4948
 
-###########################################################
-### DALVIK/ART
-###########################################################
-
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.sys.fw.dex2oat_thread_count=4 \
-    dalvik.vm.image-dex2oat-filter=speed \
-    dalvik.vm.dex2oat-filter=speed
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
-    dalvik.vm.heapmaxfree=8m
-
-###########################################################
-### HWUI
-###########################################################
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=88 \
-    ro.hwui.layer_cache_size=58 \
-    ro.hwui.path_cache_size=16 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.shape_cache_size=4 \
-    ro.hwui.gradient_cache_size=2 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=4096 \
-    ro.hwui.text_large_cache_height=2048 \
-    ro.hwui.fbo_cache_size=16
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
 
 # call Samsung LSI board support package
 $(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
