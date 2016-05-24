fd31ba5242e21506dae15e50cead94671e9720a3
Christopher N. Hesse <raymanfx@gmail.com>
2016-05-24 15:32:00 +0200

    gts2: Fix touchscreen misalignment

--- system_prop.mk
+++ system_prop.mk
@@ -23,7 +23,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bq.gpu_to_cpu_unsupported=1 \
     ro.opengles.version=196609 \
-    ro.sf.lcd_density=320
+    ro.sf.lcd_density=320 \
+    ro.sf.hwrotation=270
 
 # Hwc - not used on cm/aosp
 PRODUCT_PROPERTY_OVERRIDES += \
