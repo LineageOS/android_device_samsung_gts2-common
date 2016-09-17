ab7610f1462e318ba96656466bd337d2ee173bfb
Schischu <schischu65@gmail.com>
2016-09-17 17:22:06 +0200

    Rename validityservice to ValidityService
    Change-Id: Ibe7e623a4c263b8ea8f56c6ee8da9abbbdb17ee2

--- ValidityService/Android.mk
+++ ValidityService/Android.mk
@@ -17,17 +17,19 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := ValidityService
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_JACK_ENABLED := disabled
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-Iaidl-files-under, src)
 
 include $(BUILD_PACKAGE)
