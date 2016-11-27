ed7b3af580ad50bd15a07bdf66fb81f10fb463a8
Christopher N. Hesse <raymanfx@gmail.com>
2016-11-27 20:10:42 +0100

    Revert "gts2: Remove color format override"
    This reverts commit 634d318e643628e1b99ac69170da3a47ae41e450.
    
    Change-Id: Ica8fb2de7546c15f75def3309be03f2e5b6e61f9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,9 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 # Avoid buffer swap timing issues
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
+# Mixer
+BOARD_USE_BGRA_8888 := true
+
 # HWCServices
 BOARD_USES_HWC_SERVICES := true
 
