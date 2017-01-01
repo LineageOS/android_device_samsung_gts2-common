3032c768ec4a704178cd48f94a94abf75abd2991
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-01 19:17:31 +0100

    gts2: Drop dalvik/art speed filter flag
    Android 7 uses ART in JIT mode, which is disabled by setting it to
    speed explicitly, so we end up missing out on optimization.
    
    Change-Id: I851b4d2283ff3ed38f237dd26fda42511f6e07f7

--- system_prop.mk
+++ system_prop.mk
@@ -1,8 +1,6 @@
 # Dalvik/Art
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=4 \
-    dalvik.vm.image-dex2oat-filter=speed \
-    dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
