412565e3a55b2966857314dffa1f73ba27287400
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-15 20:56:56 +0200

    gts2: Advertise OpenGL ES 3.2
    Change-Id: Ib86d7daaaf92ea10dc7e43efe4369e285618e98c

--- system_prop.mk
+++ system_prop.mk
@@ -24,7 +24,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bq.gpu_to_cpu_unsupported=1 \
-    ro.opengles.version=196609 \
+    ro.opengles.version=196610 \
     ro.sf.lcd_density=320
 
 # Hwc - not used on cm/aosp
